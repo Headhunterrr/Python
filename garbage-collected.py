3012b35a1f972e7a50dad206139232ffc2e608f1
Your Name <you@example.com>
2025-09-18 16:47:18 +0300

    2025-09-18 v2

--- garbage-collected.py
+++ garbage-collected.py
@@ -2,11 +2,11 @@ x = [2]
 print(id(x))
 print(x)
 y = x[:]
-print(id(y))
+# print(id(y))
 x = y[:]
-print(id(x))
+# print(id(x))
 y = [3]
 print(id(y))
 print(y)
-x = y[:]
-print(id(x))
+# x = y[:]
+# print(id(x))

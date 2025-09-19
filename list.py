638eabf8f08d646dfe604595dd48ab300a9537b2
Your Name <you@example.com>
2025-09-19 15:52:52 +0300

    2025-09-18 v2

--- list.py
+++ list.py
@@ -3,37 +3,40 @@ ls1 = ['hhh', 5.6]
 print(ls)
 print(ls1, "\n")
 
-ls.append(7.5)
+ls.append(7.5)  # добовляет объект в конец списка
 print(ls)
 print(ls1, "\n")
 
-ls1.extend(ls)
-print(ls)
+ls1.extend(ls)  # добовляет объекты списка
+print(ls)      # в конец другого списка
 print(ls1, "\n")
 
-ls1.remove(23)
+ls1.remove(23)  # удаляет первый объект 23 из списка
 print(ls1)
 ls1.remove(23)
 print(ls1, "\n")
 
-ls.pop(2)
-print(ls)
+ls.pop(2)      # удаляет объект с индексом 2
+print(ls)      # и возвращает его значение
 print(ls, ls.pop(), "\n")
 
 print(ls1.index(5.6, 1, 5), "\n")
+# возвращает индекс объекта 5.6,
+# ищего его в пределах от 1 до 5 не включительно
 
 ls.append(1)
 print(ls)
 print(ls.count(1), "\n")
+# возвращает количество объектов 1
 
 ls2 = [3, 4, 1, 2.0, 2.3, 9]
 print(ls2, "\n")
 
-ls2.sort()
+ls2.sort()     # переставляет объекты в списке
+print(ls2, "\n")  # по возрвствнию
+
+ls2.reverse()  # реверсирует список
 print(ls2, "\n")
 
-ls2.reverse()
-print(ls2, "\n")
-
-ls1.clear()
+ls1.clear()  # удаляет все объекты в списке
 print(ls1, "\n")

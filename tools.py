4e7195c212123d1ec300f0a79fcecfc5afa63b60
Vyacheslav Kamilov <zycheslav.K.2000@gmail.com>
2025-09-25 20:33:18 +0300

    v13 25-09-2025

--- tools.py
+++ tools.py
@@ -2,7 +2,7 @@ a, b = 1, 1
 while a < 10:  # последовательность Фибоначи
     a, b = b, a+b
     if a < 10:
-        print(a, end=' -> ')
+        print(f"{a} --> ", end="")
     else:
         print(a, '\n')
 
@@ -10,12 +10,26 @@ while a < 10:  # последовательность Фибоначи
 fruits = {'banana': 2, 'apple': 49, 'lime': 37}
 for fruit, amount in fruits.items():
     if amount <= 10:
-        print('Few', fruit + "s")
+        print(f"Few {fruit}s")
     else:
-        print('Many', fruit + "s")
+        print(f"Many {fruit}s")
 print()
 
 # цикл for для списка
-list_number = [64, 7, 26, 133, 98, 23, 89]
-for i in list_number:
-    print(f"{list_number.index(i) + 1}'s number - {i}")
+list_numbers = [64, 7, 26, 133, 98, 23, 89]
+for i in list_numbers:
+    print(f"{list_numbers.index(i) + 1}'s number - {i}")
+print()
+
+# нумерация списка в словарь
+# начиная с единицы
+list_numbers_index_dict = dict(enumerate(list_numbers, 1))
+for i in list_numbers_index_dict:
+    print(f"{i} : {list_numbers_index_dict[i]}")
+print()
+
+# нумерация списка в список из списков
+# начиная с пятёрки
+list_numbers_index_list = list(enumerate(list_numbers, 5))
+for i in list_numbers_index_list:
+    print(i)
